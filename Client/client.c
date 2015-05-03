4056698c99da2ae39039ddca1a7c3470bc7c170f
Nazareno <nazareno.anselmi@gmail.com>
2015-05-02 23:37:01 -0300

    Cree un nuevo server que acepta multiples conexiones de clientes, y les envia el mismo mensaje que recibe de cada cliente. Tambien cree un cliente que se conecta y envia todos los mensajes que se escriban por pantalla, tiene algunos bugs aun.

--- Client/client.c
+++ Client/client.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+#define PORT 4000
 
 int main (void){
-	printf ("Hola Disney!!!!\n");
-	printf ("Hola Mundito querido!!!\n");
+	int sockfd;
+	char texto[200];
+	struct sockaddr_in server;
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror ("socket");
+		exit (1);
+	}
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("172.16.0.115");
+	server.sin_port = htons(PORT);
+
+	if (connect(sockfd, (struct sockaddr *)&server,sizeof(struct sockaddr)) == -1) {
+		perror ("connect");
+		exit (1);
+	}
+	while (1){
+		strcpy (texto,"");
+		printf ("Ingrese texto a enviar: ");
+		//scanf ("%[^\n]" , texto);
+		scanf ("%s" , texto);
+		fflush(stdin);
+		if (send(sockfd, texto, strlen(texto), 0) == -1)
+				perror ("send");
+	}
+	close (sockfd);
 	return 0;
 }
--- Server/server.c
+++ Server/server.c
@@ -1,7 +1,115 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main (void){
-	printf ("Hola Server!\n");
-	return 0;
+void *connection_handler(void *);
+
+int main(int argc , char *argv[])
+{
+    int socket_desc , new_socket , c , *new_sock;
+    struct sockaddr_in server , client;
+    char *message;
+
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        perror("socket");
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("bind");
+        exit (-1);
+    }
+    puts("Bind Exitoso");
+
+    if (listen(socket_desc , 3) == -1 ) {
+    	perror ("listen");
+    	exit (-1);
+    }
+
+    puts("Esperando conexiones...");
+    c = sizeof(struct sockaddr_in);
+    while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        puts("Connection aceptada");
+
+        message = "Hola cliente nuevo! te asigno un handler especial para vos!\n";
+        if (send(new_socket, message, strlen(message), 0) == -1) {
+        	perror("send:");
+        	exit(1);
+        }
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = new_socket;
+
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+        pthread_join( sniffer_thread , NULL);
+        puts("Handler asignado");
+    }
+
+    if (new_socket<0)
+    {
+        perror("accept");
+        exit (-1);
+    }
+    return 0;
+}
+
+/*
+ * Handler para cada nueva conexion
+ * */
+void *connection_handler(void *socket_desc)
+{
+    //Obtengo el descriptor del socket que pase por parametro
+    int sock = *(int*)socket_desc;
+    int read_size;
+    char *message , client_message[2000];
+
+    //Envio mensaje al cliente
+    message = "Bienvenido! yo sere tu handler!\n";
+    if (send(sock, message, strlen(message), 0) == -1) {
+    	perror ("send");
+    	exit (-1);
+    }
+    message = "Escribe algo por pantalla, yo lo leere y te lo volvere a enviar \n";
+    if (send(sock, message, strlen(message), 0) == -1) {
+        	perror ("send");
+        	exit (-1);
+    }
+    //Recibir mensaje del cliente
+    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+    {
+        //Le envio al cliente el mismo mensaje que me envio a mi
+        if (send(sock, client_message, strlen(client_message), 0) == -1) {
+    	    	perror ("send");
+    	    	exit (-1);
+    	}
+    }
+
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+
+    //Libero el puntero al descriptor del socket
+    free(socket_desc);
+    return 0;
 }
